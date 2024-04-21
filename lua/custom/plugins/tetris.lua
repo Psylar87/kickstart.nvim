67b72bd210eb4affa3848aad8309a6cd38fc0339
Brandon Card <bozo3887@gmail.com>
2024-04-21 12:55:53 -0400

    added tetris plugin

---
+++ lua/custom/plugins/tetris.lua
@@ -0,0 +1,8 @@
+return{
+    "ActionScripted/tetris.nvim",
+    cmd = { "Tetris" },
+    keys = { { "<cmd>Tetris<cr>", desc = "Tetris" } },
+    opts = {
+    -- your awesome configuration here
+    },
+}

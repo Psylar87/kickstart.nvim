2d019eefed06c593b543aa1af4b551769fb4958b
Brandon Card <bozo3887@gmail.com>
2024-04-21 00:27:13 -0400

    added toggleterm

---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return{
+    {'akinsho/toggleterm.nvim', version = "*", opts = {
+        open_mapping = [[<c-\>]],
+        size = 10,
+        hide_numbers = true,
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = true,
+        persist_size = true,
+        direction = 'float',
+    }},
+}

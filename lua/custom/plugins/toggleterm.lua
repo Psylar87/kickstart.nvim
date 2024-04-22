1ec8298746319d7ea3e59f887b4a31cef1be07fe
Brandon Card <bozo3887@gmail.com>
2024-04-22 15:06:08 -0400

    added comments to toggleterm

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -5,20 +5,20 @@ return {
     opts = {
       open_mapping = [[<c-\>]],
       size = 10,
-      hide_numbers = true,
-      shade_filetypes = {},
-      shade_terminals = true,
-      shading_factor = 2,
+      hide_numbers = true, -- hide the number column in toggleterm buffers
+      shade_filetypes = {}, -- this table is to disable filetype highlights for this terminal, add to it if you want more to be disabled
+      shade_terminals = false, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false.
+      shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light backgrounds
       start_in_insert = true,
-      persist_size = true,
-      close_on_exit = true,
-      direction = 'float',
+      persist_size = true, -- persist size when hidden
+      close_on_exit = true, -- close on exit
+      direction = 'float', -- | 'horizontal' | 'vertical' | 'tab' | 'float',
       float_opts = {
-        border = 'curved',
+        border = 'curved', -- | 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
         winblend = 10, -- transparency
         highlights = {
-          border = 'Normal',
-          background = 'Normal',
+          border = 'Normal', -- | 'FloatBorder' | 'Normal'
+          background = 'Normal', -- | 'FloatBg' | 'Normal'
         },
       },
     },

98171afe2089361b2dee87123f6386ef89d39001
Brandon Card <bozo3887@gmail.com>
2024-04-22 13:40:12 -0400

    added transparency to toggleterm

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,22 +1,27 @@
-return{
-    {'akinsho/toggleterm.nvim', version = "*", opts = {
-        open_mapping = [[<c-\>]],
-        size = 20,
-        hide_numbers = true,
-        shade_filetypes = {},
-        shade_terminals = true,
-        shading_factor = 2,
-        start_in_insert = true,
-        persist_size = true,
-        close_on_exit = true,
-        direction = 'float',
-        float_opts = {
-            border = 'curved',
-            winblend = 0,
-            highlights = {
-                border = "Normal",
-                background = "Normal",
-            }
-        }
-    }},
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {
+      open_mapping = [[<c-\>]],
+      size = 10,
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      persist_size = true,
+      close_on_exit = true,
+      direction = 'float',
+      float_opts = {
+        border = 'curved',
+        winblend = 10, -- transparency
+        highlights = {
+          border = 'Normal',
+          background = 'Normal',
+        },
+      },
+    },
+  },
 }
+

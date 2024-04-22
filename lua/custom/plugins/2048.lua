c80b963858c8106e918510a644a52e8fe210393a
Brandon Card <bozo3887@gmail.com>
2024-04-21 21:54:35 -0400

    edit to 2048

--- lua/custom/plugins/2048.lua
+++ lua/custom/plugins/2048.lua
@@ -2,4 +2,8 @@ return{
     "NStefan002/2048.nvim",
     cmd = "Play2048",
     config = true,
+    keys = { { "<cmd>Play2048<cr>", desc = "Play 2048" } },
+    opts = {
+    -- your awesome configuration here
+    },
 }

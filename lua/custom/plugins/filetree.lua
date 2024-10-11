1ca02d72e7e7e01adec88f9d21573f4d11746e39
Brandon Card <bozo3887@gmail.com>
2024-10-11 02:48:50 -0400

    minor tweaks

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -5,7 +5,7 @@ return {
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
   },
   config = function()
@@ -13,7 +13,9 @@ return {
       close_if_last_window = true,
       filesystem = {
         hijack_netrw_behavior = 'open_default',
-        follow_current_file = true,
+        follow_current_file = {
+          enabled = true,
+        },
         filtered_items = {
           visible = true,
           show_hidden_count = true,
@@ -25,6 +27,16 @@ return {
         position = 'right',
         width = 27,
         auto_expand_width = true,
+      },
+      event_handlers = {
+        {
+          event = 'neo_tree_window_after_open',
+          handler = function(args)
+            if args.position == 'left' or args.position == 'right' then
+              vim.cmd 'wincmd p'
+            end
+          end,
+        },
       },
     }
   end,

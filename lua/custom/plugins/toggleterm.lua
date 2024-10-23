489f9b0dd50860cc700a378a11d0a284791e32ac
Brandon Card <bozo3887@gmail.com>
2024-10-22 23:32:49 -0400

    toggleterm update

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -5,22 +5,32 @@ return {
     opts = {
       open_mapping = [[<c-\>]],
       size = 10,
-      hide_numbers = true, -- hide the number column in toggleterm buffers
-      shade_filetypes = {}, -- this table is to disable filetype highlights for this terminal, add to it if you want more to be disabled
-      shade_terminals = false, -- NOTE: this option takes priority over highlights specified so if you specify Normal highlights you should set this to false.
-      shading_factor = 1, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light backgrounds
+      hide_numbers = true,
+      shade_filetypes = {},
+      shade_terminals = true,
+      shading_factor = 2,
       start_in_insert = true,
-      persist_size = true, -- persist size when hidden
-      close_on_exit = true, -- close on exit
-      direction = 'float', -- | 'horizontal' | 'vertical' | 'tab' | 'float',
+      persist_size = true,
+      close_on_exit = true,
+      direction = 'float',
       float_opts = {
-        border = 'curved', -- | 'single' | 'double' | 'shadow' | 'curved' | ... other options supported by win open
-        winblend = 10, -- transparency
+        border = 'curved',
+        winblend = 80,
         highlights = {
-          border = 'Normal', -- | 'FloatBorder' | 'Normal'
-          background = 'Normal', -- | 'FloatBg' | 'Normal'
+          border = 'Normal',
+          background = 'Normal',
         },
       },
     },
+    config = function(_, opts)
+      require('toggleterm').setup(opts)
+      vim.api.nvim_create_autocmd('TermOpen', {
+        pattern = 'term://*toggleterm#*',
+        callback = function()
+          vim.cmd 'setlocal winblend=30'
+          vim.cmd 'setlocal winhl=Normal:Normal'
+        end,
+      })
+    end,
   },
 }

b3f8b4f672b70b341b8234e24a02102a47973bef
Brandon Card <bozo3887@gmail.com>
2024-10-24 23:40:29 -0400

    tetris update

--- lua/custom/plugins/tetris.lua
+++ lua/custom/plugins/tetris.lua
@@ -1,8 +1,6 @@
-return{
+return {
     "ActionScripted/tetris.nvim",
     cmd = { "Tetris" },
     keys = { { "<cmd>Tetris<cr>", desc = "Tetris" } },
-    opts = {
-    -- your awesome configuration here
-    },
+    opts = {},
 }
